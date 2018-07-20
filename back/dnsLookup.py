a4af72eb66d0888e43be876ce68aebb4d4cb1179
kohei <kohei@proudit.jp>
2018-07-20 15:53:28 +0900

    modified:   dnsLookup.py

--- back/dnsLookup.py
+++ back/dnsLookup.py
@@ -1,12 +1,11 @@
-# checkURL.py
-# Get URLs from DynamoDB.
-# Check the status of URL.
-# Write the status to DynamoDB.
+# dnsLookup.py
+# Check the domain is registered in DNS.
 
 import socket
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 import logging
+import datetime
 
 _dynamodb_ = boto3.resource('dynamodb')
 _starget_ = _dynamodb_.Table('S-Target')
@@ -18,16 +17,37 @@ _stjson_ = _scan_['Items']  # スキャン結果をJsonデータへ変換
 _list_ = len(_stjson_)  # 配列数を取得
 
 for i in range(0,_list_):
+  _domain_ = _stjson_[i]['domain']
+  _name_ = _stjson_[i]['name']
+  _dns_ = _stjson_[i]['dns']
+  _date_ = str(datetime.datetime.now())
   try:
-    _domain_ = _stjson_[i]['domain']
-    _addr1_ = socket.gethostbyname(_domain_)
-    _addr2_ = socket.gethostbyname('google.c')
+    _addr_ = socket.gethostbyname(_domain_)
   except Exception as e:
-#    logging.info(e.response['Error']['Message'])
-    print(_addr1_)
-    print(_addr2_)
+    logging.info(e.response['Error']['Message'])
+    res = _starget_.update_item(
+      Key={
+        'domain': _domain_,
+        'name': _name_
+      },
+      UpdateExpression='SET dns = :vall',
+      ExpressionAttributeValues={
+        ':vall': "NG"
+      }
+    )
+    print(res)
   else:
-    print(_addr1_)
-    print(_addr2_)
+    res = _starget_.update_item(
+      Key={
+        'domain': _domain_,
+        'name': _name_
+      },
+      UpdateExpression='SET dns = :dns, udate = :udate ',
+      ExpressionAttributeValues={
+        ':dns': "OK",
+        ':udate': _date_
+      }
+    )
+    print(res)
 
 #return res
